7606725152fc3cdd622ac036cfaf16f495b8662c
Lance Ziegler <lance.ziegler@hotmail.com>
2023-08-15 00:07:45 -0700

    Removed embedded repositories from frontent and backend

---
+++ nutrition_site/src/app/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+function Page() {
+  const [message, setMessage] = useState('Loading...');
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/home')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+        setPeople(data.people);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>{message}</div>
+      {people.map((person, index) => (
+        <div key={index}>{person}</div>
+      ))}
+    </>
+  );
+}
+
+export default Page;
